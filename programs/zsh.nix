531e7c626ca5f72cf5d93b99a3718d386d2a63db
whtsht <whiteshirt0079@gmail.com>
2025-04-06 23:12:14 +0900

    add open command
    https://zenn.dev/kaityo256/articles/open_command_on_wsl

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -37,6 +37,18 @@
 
       eval "$(starship init zsh)"
       eval "$(zoxide init zsh)"
+
+      function open() {
+        if [ $# != 1 ]; then
+          explorer.exe .
+        else
+          if [ -e $1 ]; then
+            explorer.exe $(wslpath -w $1) 2> /dev/null
+          else
+            echo "open: $1 : No such file or directory" 
+          fi
+        fi
+      }
     '';
   };
 } 
